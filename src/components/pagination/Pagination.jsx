f1b666e91a69b7fe2b6fe238375d1760ad9172f0
LimByeongKuk <bye0414@naver.com>
2025-12-09 15:48:01 +0900

    #261 - 상품 목록 페이지네이션 적용
    - 만들어놓은 페이지네이션 컴포넌트를 이용하여 페이지네이션 기능 구현
    - 페이지네이션 컴포넌트 일부 수정
      - 기존의 서치파라미터를 가져온 뒤 추가할것이 있다면 추가하는 로직 추가

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,25 +1,29 @@
 import {
   createSearchParams,
+  useLocation,
   useNavigate,
   useSearchParams,
 } from "react-router-dom";
 
 const Pagination = ({ pageResponseDTO }) => {
+  const [searchParams] = useSearchParams();
+  const location = useLocation();
+
   //props로 백엔드의 PageResponseDTO를 받음
 
   const navigate = useNavigate();
-  const [queryParams] = useSearchParams();
+  // const [queryParams] = useSearchParams();
   //URL 쿼리스트링 읽기 (?page=1&size=10)
 
   const {
     //ReviewPageResponseDTO 정보 구조 분해 할당
     totalDataCount, // totalReviews에 해당
     pageNumList, // 페이지 버튼 목록 (1~10, 11~20 등)
-    prevPageGroup, // 이전 페이지 그룹 존재 여부
-    nextPageGroup, // 다음 페이지 그룹 존재 여부
+    hasPrevPageGroup, // 이전 페이지 그룹 존재 여부
+    hasNextPageGroup, // 다음 페이지 그룹 존재 여부
     prevPage, // < 버튼 클릭 시 이동할 페이지
     nextPage, // > 버튼 클릭 시 이동할 페이지
-    reviewPageRequestDTO, // page, size 정보 포함
+    pageRequestDTO, // page, size 정보 포함
   } = pageResponseDTO;
 
   const getNum = (param, defaultValue) => {
@@ -28,8 +32,10 @@ const Pagination = ({ pageResponseDTO }) => {
     return parseInt(param, 10); //문자열 -> 10진수 숫자 변환
   };
 
-  const page = reviewPageRequestDTO.page || 1;
-  const size = reviewPageRequestDTO.size || 10;
+  const page = pageRequestDTO?.page || 1;
+  const size = pageRequestDTO?.size || 10;
+
+  console.log("page : ", page);
 
   const totalPage = totalDataCount > 0 ? Math.ceil(totalDataCount / size) : 1;
   //총 페이지 수 계산 , totalDataCount가 0이어도 최소 1페이지 표시
@@ -38,16 +44,21 @@ const Pagination = ({ pageResponseDTO }) => {
     //페이지 범위 (1 ~ totalPage)로 제한
     if (pageNum < 1) pageNum = 1; //최소값: 1페이지
     if (pageNum > totalPage) pageNum = totalPage; //최대값: 마지막 페이지
+    console.log("pageNum : ", pageNum);
 
-    const newParams = {
-      page: pageNum,
-      size,
-      sort: queryParams.get("sort") || "latest", // sort가 없으면 기본값 'latest'
-    };
+    // 기존의 서치파라미터를 가져온 뒤 추가할것이 있다면 추가하는 로직
+    const newParams = new URLSearchParams(searchParams.toString());
+    newParams.set("page", pageNum.toString());
+    if (!newParams.get("sort")) {
+      newParams.set("sort", "latest");
+    }
+    const queryStr = newParams.toString();
 
-    const queryStr = createSearchParams(newParams).toString();
-    //현재 페이지와 사이즈 조합을 문자열로 만들어 저장 "page=1&size=10"
-    navigate({ search: queryStr });
+    //현재 페이지경로에 서치 파라미터를 붙여서 이동
+    navigate({
+      pathname: location.pathname,
+      search: queryStr,
+    });
   };
 
   return (
@@ -61,7 +72,7 @@ const Pagination = ({ pageResponseDTO }) => {
       </button>
 
       {/* < 이전 페이지 그룹 이동 */}
-      {prevPageGroup && ( // 이전 페이지 그룹이 있을 때만 버튼 표시
+      {hasPrevPageGroup && ( // 이전 페이지 그룹이 있을 때만 버튼 표시
         <button
           className="px-3 py-1 border border-zinc-300 rounded text-sm cursor-pointer"
           // prevPage는 ReviewPageResponseDTO에서 계산된 값
@@ -72,7 +83,7 @@ const Pagination = ({ pageResponseDTO }) => {
       )}
 
       {/* 페이지 번호 렌더링: pageNumList 사용 */}
-      {pageNumList.map((num) => (
+      {pageNumList?.map((num) => (
         <button
           key={num}
           className={`px-3 py-1 border border-zinc-300 rounded text-sm cursor-pointer ${
@@ -85,10 +96,10 @@ const Pagination = ({ pageResponseDTO }) => {
       ))}
 
       {/* > 다음 페이지 그룹 이동 */}
-      {nextPageGroup && ( // 다음 페이지 그룹이 있을 때만 버튼 표시
+      {hasNextPageGroup && ( // 다음 페이지 그룹이 있을 때만 버튼 표시
         <button
           className="px-3 py-1 border border-zinc-300 rounded text-sm cursor-pointer"
-          // nextPage는 ReviewPageResponseDTO에서 계산된 값
+          // nextPage는 PageResponseDTO에서 계산된 값
           onClick={() => moveToPage(nextPage)}
         >
           {">"}
