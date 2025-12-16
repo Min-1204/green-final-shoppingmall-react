19bd263853201b8d2959dbbbaf6f6918fe324e96
Shinsora <cogmo18@gmail.com>
2025-12-16 22:27:23 +0900

    #286 - [프론트] 검색 결과 페이지 페이징
    - 검색API, 검색 결과 컴포넌트에 페이지 기능 추가
    - 필터, 정렬 삭제
    - 검색 아이콘 클릭으로 검색시 [object, Object]가 검색창에 뜨는 문제 수정

--- src/components/search/SearchResultComponent.jsx
+++ src/components/search/SearchResultComponent.jsx
@@ -1,47 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import ProductCard from "../product/ProductCard";
-import ProductSortBar from "../product/ProductSortBar";
-import ProductFilterBar from "../filter/ProductFilterBar";
 import { searchProductList } from "../../api/search/searchApi";
+import Pagination from "../pagination/Pagination";
 
 const SearchResultComponent = () => {
+  const [pageResponse, setPageResponse] = useState(null);
   const [products, setProducts] = useState([]); //상품 목록
-  const [filter, setFilter] = useState({});
-  const [sort, setSort] = useState(""); //정렬
-
   const [searchParams] = useSearchParams(); //URL 쿼리스트링 읽기
+  const [queryParams] = useSearchParams(); //페이지 쿼리스트링
   const keyword = searchParams.get("keyword") || ""; //URL에서 keyword 값 추출, 값이 없으면 빈 문자열
+
+  const getNum = (param, defaultValue) => {
+    if (!param) return defaultValue;
+    return parseInt(param, 10);
+  };
 
   //검색어 상품 목록 조회
   useEffect(() => {
+    const page = getNum(queryParams.get("page"), 1);
+    const size = getNum(queryParams.get("size"), 24);
+
     //공백 이거나 빈 검색어면 API 호출하지 않음
     if (!keyword.trim()) return;
 
     const fetchProducts = async () => {
-      const products = await searchProductList(keyword);
-      setProducts(products);
+      const products = await searchProductList(keyword, page, size);
+      setProducts(products.dtoList);
+      setPageResponse(products);
     };
     fetchProducts();
-  }, [keyword]);
-
-  const brandOptions = [];
-  products.forEach((product) => {
-    //브랜드 이름이 있는 경우
-    if (product.brand && product.brand.name) {
-      //이미 들어있는 브랜드가 아니면
-      if (!brandOptions.includes(product.brand.name)) {
-        brandOptions.push(product.brand.name);
-      }
-    }
-  });
-  // const brandOptions = [
-  //   //각 상품에서 브랜드 이름만 뽑기
-  //   //?. -> 브랜드가 없는 상품 대비
-  //   //.filter(boolean) : null, undefined, "" 제거
-  //   //new Set([...]) : 동일 브랜드 여러개일 경우 하나만 유지
-  //   ...new Set(products.map((p) => p.brand?.name).filter(Boolean)),
-  // ];
+  }, [keyword, queryParams]);
 
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 min-h-screen">
@@ -55,16 +44,6 @@ const SearchResultComponent = () => {
           <span className="font-semibold text-gray-900">{products.length}</span>
           개의 상품
         </p>
-      </div>
-
-      {/* 필터 & 정렬 */}
-      <div className="mb-6 p-4 bg-gray-50 rounded-xl border border-gray-200">
-        <ProductFilterBar
-          filters={filter}
-          setFilters={setFilter}
-          brandOptions={brandOptions}
-        />
-        <ProductSortBar sort={sort} setSort={setSort} />
       </div>
 
       {/* 상품 목록 */}
@@ -88,6 +67,8 @@ const SearchResultComponent = () => {
           </p>
         </div>
       )}
+      {/* 페이지네이션 컴포넌트 */}
+      {pageResponse && <Pagination pageResponseDTO={pageResponse} />}
     </div>
   );
 };
